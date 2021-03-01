e152ffbb5d72dfdc1c4328616e891e1ab70adb58
Pedro Henrique <63188003+pphdev@users.noreply.github.com>
2021-03-01 19:30:54 -0300

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,32 @@
+
+// 01)
+var nickName:String = "Pidi";
+print(nickName);
+var name = "Pedro_Henrique";
+
+
+//02)
+
+var nameComprador = "";
+var cpfComprador = "";
+let valorCarro = 639990;
+let modeloCarro = "Audi RS5";
+
+//03)
+- É uma linguagem de programação consistente e intuitiva, desenvolvida pela Apple para a criação de apps para iOS, Mac, Apple TV e Apple Watch.
+- Ela foi criada para dar ainda mais liberdade para os desenvolvedores.
+- É fácil de usar e em código aberto, para que qualquer pessoa com uma boa ideia consiga fazer coisas surpreendentes.
+//04)
+var intValue = 2
+var doubleValue = 2.4
+var valueString = String(intValue)
+var valueString = String(doubleValue)
+
+//05)
+
+//type safe: 
+// Swift é uma linguagem de tipo seguro. Uma linguagem de tipo seguro incentiva você a ser claro sobre os tipos de valores com os quais seu código pode trabalhar. Se parte do seu código espera uma String, você não pode passar um Int por engano.
+
+//type inference
+
+// Se você não especificar o tipo de valor de que precisa, o Swift usa a inferência de tipo para descobrir o tipo apropriado. A inferência de tipo permite que um compilador deduza o tipo de uma expressão específica automaticamente ao compilar seu código, simplesmente examinando os valores fornecidos.
